c408f84f61cc2582d645c430aa2d59efcbf1a1a9
andre <andremaccar@gmail.com>
2025-07-04 01:02:34 -0300

    configurando automacao estatus

--- src/components/automacoes/CardAutomacao.jsx
+++ src/components/automacoes/CardAutomacao.jsx
@@ -1,5 +1,7 @@
 // src/components/automacoes/CardAutomacao.jsx
 import React from "react";
+import React, { useState } from "react";
+
 
 export default function CardAutomacao({
   statusColuna = "Sem Contato",
